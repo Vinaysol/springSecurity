b5c500aa5d4a00d96a2814463cb1a0efb99ff09c
vinayso <vinay.solanki@amdocs.com>
2024-04-14 21:03:20 +0530

    configured codegen maven stub creation and

---
+++ src/main/java/com/vs/springSecurity/Models/Employee.java
@@ -0,0 +1,28 @@
+package com.vs.springSecurity.Models;
+
+public class Employee{
+
+    int id;
+    String Name;
+
+    public Employee(int id, String name) {
+        this.id = id;
+        Name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
